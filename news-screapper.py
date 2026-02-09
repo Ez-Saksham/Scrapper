b31e4c1488e89206f1e0ad865c42e4238bd0736d
Ez-Saksham <sakxamydv@gmail.com>
2026-02-09 14:51:20 +0000

    k news added

--- news-screapper.py
+++ news-screapper.py
@@ -6,7 +6,27 @@ response1 = requests.get(news1)
 
 
 
-soup = BeautifulSoup(response1.text,'lxml')
-print(soup.prettify())
+soup = BeautifulSoup(response1.text,'html.parser')
 
-print(soup.h1.text)
+
+match0 = soup.find('article',class_='1')
+main_headline = match0.h2.a.text
+print(main_headline)
+ 
+
+match1 = soup.find('article',class_='article-image article-image--left 3')
+news_headline = match1.h3.a.text
+print(news_headline)
+
+
+match2 = soup.find('article',class_='article-image article-image--left 2')
+news_headline = match2.h3.a.text
+print(news_headline)
+
+
+
+match3 = soup.find('article',class_='article-image article-image--left 4')
+news_headline = match3.h3.a.text
+print(news_headline)
+
+
